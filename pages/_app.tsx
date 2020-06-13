b790238ae2c53e522ba11ebb75dd0477468ddf49
Adrian Li <li.adrianmc@gmail.com>
2020-06-13 00:57:25 -0400

    convert to TS; add ethers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
-import App from 'next/app'
-import { ThemeProvider } from 'styled-components'
+import App from "next/app";
+import { ThemeProvider } from "styled-components";
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
-    )
+    );
   }
 }
