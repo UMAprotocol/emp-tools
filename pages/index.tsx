25498ea020db4ee4a2981c5a152c11e5b679e906
Adrian Li <li.adrianmc@gmail.com>
2020-06-28 16:09:55 -0400

    style the tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styled from "styled-components";
 import { Container, Box, Typography, Tab, Tabs } from "@material-ui/core";
 
 import Header from "../features/core/Header";
@@ -6,6 +7,15 @@ import ConnectionStatus from "../features/core/ConnectionStatus";
 import EmpPicker from "../features/emp-picker/EmpPicker";
 import ContractState from "../features/contract-state/ContractState";
 import ManagePosition from "../features/manage-position/ManagePosition";
+
+const StyledTabs = styled(Tabs)`
+  & .MuiTabs-flexContainer {
+    border-bottom: 1px solid #999;
+  }
+  & .Mui-selected {
+    font-weight: bold;
+  }
+`
 
 export default function Index() {
   const [tabIndex, setTabIndex] = useState(0);
@@ -15,10 +25,10 @@ export default function Index() {
         <Header />
         <ConnectionStatus />
         <EmpPicker />
-        <Tabs value={tabIndex} onChange={(_, i) => setTabIndex(i)}>
+        <StyledTabs value={tabIndex} onChange={(_, i) => setTabIndex(i)}>
           <Tab label="General Info" />
           <Tab label="Manage Position" />
-        </Tabs>
+        </StyledTabs>
         {tabIndex === 0 && (
           <>
             <Box pt={3} pl={2} maxWidth={800}>
