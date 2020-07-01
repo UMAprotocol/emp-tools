03291ce648193e8d8b06c60bd45edbe84dacdaad
Adrian Li <li.adrianmc@gmail.com>
2020-07-01 15:59:47 -0400

    refactor Totals container

--- containers/Totals.ts
+++ containers/Totals.ts
@@ -1,51 +1,45 @@
 import { createContainer } from "unstated-next";
 import { useState, useEffect } from "react";
 import { ethers, BigNumberish } from "ethers";
+
 import EmpState from "./EmpState";
-import Token from "./Token";
 import Collateral from "./Collateral";
+import Token from "./Token";
 
 const fromWei = ethers.utils.formatUnits;
-const weiToNum = (x: BigNumberish, decimals = 18) =>
-  parseFloat(fromWei(x, decimals));
+const weiToNum = (x: BigNumberish, u = 18) => parseFloat(fromWei(x, u));
 
 function useTotals() {
   const { empState } = EmpState.useContainer();
   const { decimals: collDec } = Collateral.useContainer();
   const { decimals: tokenDec } = Token.useContainer();
+
   const [totalCollateral, setTotalCollateral] = useState<number | null>(null);
   const [totalTokens, setTotalTokens] = useState<number | null>(null);
   const [gcr, setGCR] = useState<number | null>(null);
 
+  const {
+    cumulativeFeeMultiplier: multiplier,
+    rawTotalPositionCollateral: rawColl,
+    totalTokensOutstanding: totalTokensWei,
+  } = empState;
+
   // set GCR when state updates
   useEffect(() => {
-    const {
-      cumulativeFeeMultiplier: multiplier,
-      rawTotalPositionCollateral: rawColl,
-      totalTokensOutstanding: totalTokensWei,
-    } = empState;
+    if (multiplier && rawColl && totalTokensWei && collDec && tokenDec) {
+      // use multiplier to find real total collateral in EMP
+      const totalColl = weiToNum(multiplier) * weiToNum(rawColl, collDec);
+      const totalTokens = weiToNum(totalTokensWei, tokenDec);
+      const gcr = totalColl / totalTokens;
 
-    if (collDec && tokenDec) {
-      const totalColl =
-        multiplier && rawColl
-          ? weiToNum(multiplier) * weiToNum(rawColl, collDec)
-          : null;
-      const totalTokens = totalTokensWei
-        ? weiToNum(totalTokensWei, tokenDec)
-        : null;
-      const gcr = totalColl && totalTokens ? totalColl / totalTokens : null;
-
+      // set states
       setTotalCollateral(totalColl);
       setTotalTokens(totalTokens);
       setGCR(gcr);
     }
   }, [empState, collDec, tokenDec]);
 
-  return {
-    totalCollateral,
-    totalTokens,
-    gcr,
-  };
+  return { totalCollateral, totalTokens, gcr };
 }
 
 const Totals = createContainer(useTotals);
