86fd12c81d5cc358148de9df7c36835561f10caa
Adrian Li <li.adrianmc@gmail.com>
2020-07-01 01:19:39 -0400

    refactor allowance and approval of token

--- containers/Token.ts
+++ containers/Token.ts
@@ -5,9 +5,13 @@ import erc20 from "@studydefi/money-legos/erc20";
 
 import Connection from "./Connection";
 import EmpState from "./EmpState";
+import EmpAddress from "./EmpAddress";
+
+const fromWei = ethers.utils.formatUnits;
 
 function useToken() {
   const { signer, address, block$ } = Connection.useContainer();
+  const { empAddress } = EmpAddress.useContainer();
   const { empState } = EmpState.useContainer();
   const tokenAddress = empState.tokenCurrency;
 
@@ -15,6 +19,7 @@ function useToken() {
   const [symbol, setSymbol] = useState<string | null>(null);
   const [name, setName] = useState<string | null>(null);
   const [decimals, setDecimals] = useState<number | null>(null);
+  const [allowance, setAllowance] = useState<number | "Infinity" | null>(null);
   const [balance, setBalance] = useState<string | null>(null);
 
   const getTokenInfo = async () => {
@@ -22,13 +27,28 @@ function useToken() {
       const symbol = await contract.symbol();
       const name = await contract.name();
       const decimals = await contract.decimals();
+      const allowanceRaw = await contract.allowance(address, empAddress);
+      const allowance = allowanceRaw.eq(ethers.constants.MaxUint256)
+        ? "Infinity"
+        : parseFloat(fromWei(allowanceRaw, decimals));
       const balanceRaw = await contract.balanceOf(address);
       const balance = ethers.utils.formatUnits(balanceRaw, decimals);
 
       setSymbol(symbol);
       setName(name);
       setDecimals(decimals);
+      setAllowance(allowance);
       setBalance(balance);
+    }
+  };
+
+  const setMaxAllowance = async () => {
+    if (contract && empAddress) {
+      try {
+        await contract.approve(empAddress, ethers.constants.MaxUint256);
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
@@ -37,6 +57,7 @@ function useToken() {
     setSymbol(null);
     setName(null);
     setDecimals(null);
+    setAllowance(null);
     setBalance(null);
     getTokenInfo();
   }, [contract]);
@@ -62,8 +83,10 @@ function useToken() {
     name,
     symbol,
     decimals,
+    allowance,
     balance,
     address: tokenAddress,
+    setMaxAllowance,
   };
 }
 
