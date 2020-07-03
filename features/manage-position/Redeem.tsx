4e7d4d2d317a5dcca4e061ed709753b3bb45cb3a
Matt Rice <matthewcrice32@gmail.com>
2020-07-02 20:15:33 -0400

    fix type error
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- features/manage-position/Redeem.tsx
+++ features/manage-position/Redeem.tsx
@@ -25,7 +25,7 @@ const Redeem = () => {
 
   const tokensToRedeemFloat = isNaN(parseFloat(tokensToRedeem)) ? 0 : parseFloat(tokensToRedeem);
   const isEmpty = tokensToRedeem === "";
-  const canSendTxn = !isNaN(parseFloat(tokensToRedeem)) && tokensToRedeemFloat >= 0 && tokensToRedeemFloat <= borrowedTokens;
+  const canSendTxn = !isNaN(parseFloat(tokensToRedeem)) && tokensToRedeemFloat >= 0 && tokensToRedeemFloat <= (borrowedTokens || 0);
 
   const needAllowance = () => {
     if (syntheticAllowance === null || tokensToRedeem === null) return true;
