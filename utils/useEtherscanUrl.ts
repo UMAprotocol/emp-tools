3c4cac61bc66a8d7af9dbb1da7fdc889613c3257
Christopher Maree <christopher.maree@gmail.com>
2020-07-21 13:12:26 +0200

    reverted etherscan util

--- utils/useEtherscanUrl.ts
+++ utils/useEtherscanUrl.ts
@@ -12,12 +12,21 @@ const NETWORK_MAP: NetworkMap = {
   mainnet: "https://etherscan.io",
 };
 
-export const useEtherscanUrl = (txnHash: string | null) => {
+export const useEtherscanUrl = (hex: string | null) => {
   const { network } = Connection.useContainer();
-  if (!txnHash || !network) {
+
+  if (!network || !hex || !network.name) {
     return undefined;
   }
-
   const baseUrl = NETWORK_MAP[network.name] || NETWORK_MAP["mainnet"];
-  return `${baseUrl}/tx/${txnHash}`;
+  // If the length is 66 then the hex is a transaction.
+  if (hex.length == 66) {
+    return `${baseUrl}/tx/${hex}`;
+  }
+  // If the length is 42 then the hex is an address.
+  if (hex.length == 42) {
+    return `${baseUrl}/address/${hex}`;
+  }
+  // If the length is neither 66 or 42 then it is invalid.
+  return undefined;
 };
