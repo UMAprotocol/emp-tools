aae695632a8842c693182694789dbe7562bd109c
Nick Pai <npai.nyc@gmail.com>
2020-07-23 18:44:39 -0400

    remove export from non importable contracts
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- utils/getExchangeLinks.ts
+++ utils/getExchangeLinks.ts
@@ -6,37 +6,38 @@ interface ExchangeNameMap {
   [exchangeType: number]: string;
 }
 
-export enum EXCHANGES {
-  UNISWAP,
-  BALANCER,
-}
-
-export const EXCHANGE_NAMES_MAP: ExchangeNameMap = {
-  [EXCHANGES.UNISWAP]: "Uniswap",
-  [EXCHANGES.BALANCER]: "Balancer",
-};
-
-// Returns a function for a given exchange that can be used to retrieve an exchange's specific swap URL for a chosen token.
 interface ExchangeLinkMap {
   [exchangeEnum: number]: (params: any) => string;
 }
 
-// A mapping of synthetic tokens to the exchange where they can be traded.
-export const TOKEN_TO_EXCHANGE_MAP: TokenExchangeMap = {
+enum EXCHANGES {
+  UNISWAP,
+  BALANCER,
+}
+
+const EXCHANGE_NAMES_MAP: ExchangeNameMap = {
+  [EXCHANGES.UNISWAP]: "Uniswap",
+  [EXCHANGES.BALANCER]: "Balancer",
+};
+
+// Maps synthetic tokens to the exchange where they and their collateral can be traded.
+const TOKEN_TO_EXCHANGE_MAP: TokenExchangeMap = {
   ycomp: EXCHANGES.UNISWAP,
   ethbtc: EXCHANGES.UNISWAP,
   yusd: EXCHANGES.BALANCER,
 };
 
-export const EXCHANGE_LINK_MAP: ExchangeLinkMap = {
+// Maps exchange types to functions that can be used to retrieve the exchange's swap URL for a chosen token.
+const EXCHANGE_LINK_MAP: ExchangeLinkMap = {
   [EXCHANGES.UNISWAP]: (tokenAddress) =>
     `https://app.uniswap.org/#/swap?outputCurrency=${tokenAddress}`,
   [EXCHANGES.BALANCER]: (tokenAddress) =>
     `https://balancer.exchange/#/swap/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48/${tokenAddress}`,
-  // This returns a Balancer exchange URL to buy `tokenAddress` and sell USDC, a well understood currency
+  // Since two currencies must be specified to the Balancer server,
+  // This returns a Balancer exchange URL to swap `tokenAddress` and sell USDC, a well understood currency
 };
 
-export const getExchangeTypeFromTokenSymbol = (symbol: string | null) => {
+const _getExchangeTypeFromTokenSymbol = (symbol: string | null) => {
   switch (true) {
     case symbol?.includes("yCOMP"):
       return TOKEN_TO_EXCHANGE_MAP.ycomp;
@@ -50,7 +51,7 @@ export const getExchangeTypeFromTokenSymbol = (symbol: string | null) => {
 };
 
 export const getExchangeInfo = (symbol: string | null) => {
-  const exchangeType = getExchangeTypeFromTokenSymbol(symbol);
+  const exchangeType = _getExchangeTypeFromTokenSymbol(symbol);
   if (exchangeType !== null) {
     return {
       name: EXCHANGE_NAMES_MAP[exchangeType],
