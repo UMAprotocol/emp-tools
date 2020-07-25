3803b4f63cb5158c8bd5902f0fdaa1531ebd974a
Nick Pai <npai.nyc@gmail.com>
2020-07-25 04:21:00 -0400

    Update getLiquidationPrice.ts

--- utils/getLiquidationPrice.ts
+++ utils/getLiquidationPrice.ts
@@ -1,16 +1,9 @@
 export function getLiquidationPrice(
-  collateral: number | null,
-  tokens: number | null,
-  crThreshold: number | null
+  collateral: number,
+  tokens: number,
+  crThreshold: number
 ) {
-  if (
-    tokens === null ||
-    collateral === null ||
-    crThreshold === null ||
-    tokens <= 0 ||
-    crThreshold <= 0 ||
-    collateral <= 0
-  ) {
+  if (tokens <= 0 || crThreshold <= 0 || collateral <= 0) {
     // If liquidation price is 0, then the position can always be liquidated (i.e. it is an invalid position).
     return 0;
   }
