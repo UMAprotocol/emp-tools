8b3e6ad0f3aa5700395ae55c26d57bdc43f06a33
Nick Pai <npai.nyc@gmail.com>
2020-07-27 03:48:17 -0400

    change hlper text
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- features/yield/YieldCalculator.tsx
+++ features/yield/YieldCalculator.tsx
@@ -90,7 +90,7 @@ const YieldCalculator = () => {
                 onChange={(e) => setTokenPrice(e.target.value)}
                 variant="outlined"
                 inputProps={{ min: "0", max: "10", step: "0.01" }}
-                helperText={`Enter the market observable price`}
+                helperText={`Enter the price of yUSD in $`}
                 InputLabelProps={{
                   shrink: true,
                 }}
