7f1652ddc7bef3524546e794f24f437a3a0e59c9
Nick Pai <npai.nyc@gmail.com>
2020-07-28 23:46:31 -0400

    refactor yield calc
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- features/yield/YieldCalculator.tsx
+++ features/yield/YieldCalculator.tsx
@@ -13,55 +13,41 @@ const MS_PER_S = 1000;
 const S_PER_DAY = 60 * 60 * 24;
 const DAYS_PER_YEAR = 365;
 
-const PLACEHOLDER_PRICE = "0.9875";
-
-// TODO: If the user has not selected an EMP (or has not even connected their web3 provider yet),
-// then the EMP's expirationTimestamp will clearly be unavailable. In order to support explorability,
-// this is a placeholder `daysToExpiry` that will be the default value for the textfield if no EMP is selected yet.
-const PLACEHOLDER_DAYS_TO_EXPIRY = "30";
-
 const YieldCalculator = () => {
   const { empState } = EmpState.useContainer();
   const { expirationTimestamp } = empState;
   const { usdPrice } = Balancer.useContainer();
 
-  const [tokenPrice, setTokenPrice] = useState<string | null>(
-    usdPrice ? usdPrice.toString() : null
-  );
-  const [yieldAmount, setYieldAmount] = useState<number | null>(null);
+  const [tokenPrice, setTokenPrice] = useState<string>("");
+  const [daysToExpiry, setDaysToExpiry] = useState<string>("");
+  const [yieldAmount, setYieldAmount] = useState<string>("");
 
-  const calculateDaysToExpiry = () => {
-    if (expirationTimestamp) {
-      const currentTimestamp = Math.round(Date.now() / MS_PER_S);
-      const secondsToExpiry = expirationTimestamp.toNumber() - currentTimestamp;
+  function setEmpValues() {
+    if (expirationTimestamp !== null && usdPrice !== null) {
+      const calculateDaysToExpiry = () => {
+        const currentTimestamp = Math.round(Date.now() / MS_PER_S);
+        const secondsToExpiry =
+          expirationTimestamp.toNumber() - currentTimestamp;
 
-      return Math.round(secondsToExpiry / S_PER_DAY);
-    } else {
-      return null;
+        return Math.round(secondsToExpiry / S_PER_DAY);
+      };
+
+      const daysToExpiry = calculateDaysToExpiry();
+
+      setTokenPrice(usdPrice.toString());
+      setDaysToExpiry(daysToExpiry.toString());
     }
-  };
+  }
 
-  // We set this state var `daysToExpiry` after declaring `calculateDaysToExpiry()` because we first want to check if its value is
-  // non null in order to set its default value.
-  const [daysToExpiry, setDaysToExpiry] = useState<string | null>(
-    calculateDaysToExpiry()?.toString() || PLACEHOLDER_DAYS_TO_EXPIRY
-  );
-
-  const calculateYield = () => {
-    if (!tokenPrice || Number(tokenPrice) <= 0) {
-      return null;
-    }
-    if (!daysToExpiry || Number(daysToExpiry) <= 0) {
+  const calculateYield = (_daysToExpiry: number, _tokenPrice: number) => {
+    if (_tokenPrice <= 0 || _daysToExpiry <= 0) {
       return null;
     }
 
     // `yieldPerUnit` = (FACE/yUSD_PX)^(1/(365/DAYS_TO_EXP)) - 1,
     // where FACE = $1. More details: https://www.bankrate.com/glossary/a/apy-annual-percentage-yield/
     const yieldPerUnit =
-      Math.pow(
-        1 / Number(tokenPrice),
-        1 / (Number(daysToExpiry) / DAYS_PER_YEAR)
-      ) - 1;
+      Math.pow(1 / _tokenPrice, 1 / (_daysToExpiry / DAYS_PER_YEAR)) - 1;
     return yieldPerUnit;
   };
 
@@ -70,10 +56,19 @@ const YieldCalculator = () => {
     return (x * 100).toFixed(2);
   };
 
-  // Update the yield whenever the parameters change.
+  // Update state whenever EMP selection changes.
   useEffect(() => {
-    setYieldAmount(calculateYield());
-  }, [tokenPrice, daysToExpiry]);
+    setEmpValues();
+  }, [empState]);
+
+  // Update yield amount whenever inputs change.
+  useEffect(() => {
+    const updatedYield = calculateYield(
+      Number(daysToExpiry) || 0,
+      Number(tokenPrice) || 0
+    );
+    setYieldAmount(prettyPercentage(updatedYield));
+  }, [daysToExpiry, tokenPrice]);
 
   return (
     <span>
@@ -86,7 +81,7 @@ const YieldCalculator = () => {
                 fullWidth
                 type="number"
                 label="Current yUSD Price (USD)"
-                value={tokenPrice?.toString() || ""}
+                value={tokenPrice}
                 onChange={(e) => setTokenPrice(e.target.value)}
                 variant="outlined"
                 inputProps={{ min: "0", max: "10", step: "0.01" }}
@@ -103,14 +98,10 @@ const YieldCalculator = () => {
                 fullWidth
                 type="number"
                 label="Days to Expiry"
-                value={daysToExpiry?.toString() || ""}
+                value={daysToExpiry}
                 onChange={(e) => setDaysToExpiry(e.target.value)}
-                inputProps={{ min: "0", max: "10", step: "1" }}
-                helperText={
-                  calculateDaysToExpiry()
-                    ? `Days to expiry for chosen EMP: ${calculateDaysToExpiry()}`
-                    : ""
-                }
+                inputProps={{ min: "0", step: "1" }}
+                helperText={`Days to expiry for chosen EMP: ${daysToExpiry}`}
                 variant="outlined"
                 InputLabelProps={{
                   shrink: true,
@@ -121,8 +112,7 @@ const YieldCalculator = () => {
           <Grid item md={4} sm={12} xs={12}>
             <Box pt={1} textAlign="center">
               <Typography variant="h6">
-                Yearly APR for <strong>buyers</strong>:{" "}
-                {prettyPercentage(yieldAmount)}%
+                Yearly APR for <strong>buyers</strong>: {yieldAmount}%
               </Typography>
             </Box>
           </Grid>
